30c29fce9ec4760860f2bd95f5b958bdfcc317be
evenrom <evenrom@gmail.com>
2026-01-29 15:02:22 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'boxing-pro-v2';
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './style.css',
+      './script.js',
+      './manifest.json'
+    ]))
+  );
+});
+self.addEventListener('fetch', (e) => e.respondWith(
+  caches.match(e.request).then((response) => response || fetch(e.request))
+));
